6b4ffde13e60fd2170570035b2813ef7e4ab31a0
bandarAmed <bandarAmed>
2023-12-29 14:00:28 +0300

    first commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# My-Rcipes-App
---
+++ src/config/urls.jsx
@@ -0,0 +1,12 @@
+export const API_URL = "http://192.168.8.112:4000"
+
+export const REGISTER_URL = "account/register"
+export const LOGIN_URL = "account/login"
+export const PROFILE_URL = "account/profile"
+export const PROFILE_UPDATE_URL = "account/profile/update"
+export const UPLOAD_USER_PHOTO = "account/profile/upload-photo"
+export const GET_ALL_POSTS = 'posts'
+export const GET_MY_POSTS = 'my-post'
+export const CREATE_POST = 'post/create'
+export const UPDATE_POST = '/update'
+export const DELETE_POST = "my-post/delete"
